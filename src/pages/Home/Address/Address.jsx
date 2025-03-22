31da1f855d3ed181e9db22d39551706f305ac09f
unknown <ce.rumiakter@gmail.com>
2025-03-22 14:38:03 +0600

    add address banner section

---
+++ src/pages/Home/Address/Address.jsx
@@ -0,0 +1,32 @@
+import { RiCalendarScheduleFill } from "react-icons/ri";
+import { MdWifiCalling,MdLocationOn } from "react-icons/md";
+
+const Address = () => {
+    return (
+        <div className="container mx-auto bg-[#151515] mt-12 mb-12 px-52 py-24 rounded-2xl flex justify-between">
+            <div className="text-white flex items-center gap-4">
+                <p className="text-4xl text-[#FF3811]"><RiCalendarScheduleFill /></p>
+                <div>
+                    <p className="text-sm text-[#E8E8E8]">We are open monday-friday</p>
+                    <h1 className="text-2xl font-medium">7:00 am - 9:00 pm</h1>
+                </div>
+            </div>
+            <div className="text-white flex items-center gap-4">
+                <p className="text-4xl text-[#FF3811]"><MdWifiCalling /></p>
+                <div>
+                    <p className="text-sm text-[#E8E8E8]">Have a question?</p>
+                    <h1 className="text-2xl font-medium">+2546 251 2658</h1>
+                </div>
+            </div>
+            <div className="text-white flex items-center gap-4">
+                <p className="text-4xl text-[#FF3811]"><MdLocationOn /></p>
+                <div>
+                    <p className="text-sm text-[#E8E8E8]">Need a repair? our address</p>
+                    <h1 className="text-2xl font-medium">Liza Street, New York</h1>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Address;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,4 +1,5 @@
 import About from "../About/About";
+import Address from "../Address/Address";
 import Banner from "../Banner/Banner";
 import Services from "../Services/Services";
 
@@ -8,6 +9,7 @@ const Home = () => {
             <Banner></Banner>
             <About></About>
             <Services></Services>
+            <Address></Address>
         </div>
     );
 };
