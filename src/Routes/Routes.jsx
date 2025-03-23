86c0b164c5831cf09af606d7ecf1809bc2739d99
unknown <ce.rumiakter@gmail.com>
2025-03-23 16:03:02 +0600

    add error page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,12 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Service from "../pages/Service/Service/Service";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Main></Main>,
+        errorElement:<ErrorPage></ErrorPage>,
         children:[
             {
                 path:'/',
---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div className='flex justify-center mt-40'>
+            <img src="https://i.ibb.co.com/4w6L3D5t/Frame.png" alt="" />
+        </div>
+    );
+};
+
+export default ErrorPage;
