5b285735f401f7a71dbf77440d3b8d0582f8b60a
unknown <ce.rumiakter@gmail.com>
2025-03-23 15:58:11 +0600

    add rightside car doctor special

--- src/pages/Service/RightSide/RightSide.jsx
+++ src/pages/Service/RightSide/RightSide.jsx
@@ -27,8 +27,8 @@ const RightSide = () => {
                     <p className="text-xl text-[#FF3811]"><IoArrowForward /></p>
                 </div>
             </div>
-            <div className="bg-[#151515] p-10 rounded-xl">
-                <h2 className="text-2xl font-bold text-white mb-6">Download</h2>
+            <div className="bg-[#151515] p-10 rounded-xl mb-8">
+                <h2 className="text-2xl font-bold text-white mb-8">Download</h2>
                 <div className="flex items-center justify-between">
                     <div className="text-white flex items-center gap-2">
                         <p className="text-4xl"><LuFileChartColumn /></p>
@@ -54,6 +54,19 @@ const RightSide = () => {
                     </div>
                 </div>
             </div>
+            <div className="bg-[#151515] p-10 rounded-xl text-center space-y-6">
+                <img className="mx-auto" src="https://i.ibb.co.com/tdTVt2P/Group-2-1.png" alt="" />
+                <h2 className="text-white text-lg font-medium">Need Help? We Are Here To Help You</h2>
+                <div className="flex justify-center">
+                    <div className="bg-white w-[270px] rounded-lg px-11 py-4 space-y-2">
+                        <h2 className="text-xl font-medium">Car Doctor Special</h2>
+                        <p className="text-[#737373]">Save up to 60% off</p>
+                        <button className="btn bg-[#FF3811] border-none text-white ">Get A Quote</button>
+                    </div>
+                </div>
+            </div>
+            <h2 className="text-2xl font-bold mt-8 mb-8 text-center">Price $250.00</h2>
+            <button className="btn bg-[#FF3811] text-xl text-white w-full">Proceed Checkout</button>
         </div>
     );
 };
