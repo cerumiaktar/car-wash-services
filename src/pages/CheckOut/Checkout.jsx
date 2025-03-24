89fef61c9de205321b692815fe3f0f1c1c1df7dd
unknown <ce.rumiakter@gmail.com>
2025-03-24 11:43:31 +0600

    add checkout banner

---
+++ src/pages/CheckOut/Checkout.jsx
@@ -0,0 +1,22 @@
+
+
+const Checkout = () => {
+    return (
+        <div className="container mx-auto mt-12 mb-20">
+            <div
+                className="hero h-[300px] rounded-2xl"
+                style={{
+                    backgroundImage: "url(https://i.ibb.co.com/v6KNQH9z/Group-38775.png)",
+                }}>
+                <div className="hero-overlay rounded-2xl"></div>
+                <div className="hero-content text-neutral-content text-center">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">Checkout</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Checkout;
